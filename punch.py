3d3c6f867930f687a3d74b08fa58d65260df3439
githubmg <maria.gaska@gmail.com>
2019-01-08 19:44:08 +0000

    sec

--- punch.py
+++ punch.py
@@ -167,12 +167,27 @@ def greengrass_infinite_infer_run():
             print 'heatmapshape'
             print heatmap.shape
             
-            scaledImgRendered = Image.fromarray(scaledImg, 'RGB')
-            from scipy.ndimage.filters import gaussian_filter
             all_peaks = []
             peak_counter = 0
-
+            
+            param={}
+            param['octave'] = 3
+            param['use_gpu'] = 1
+            param['starting_range'] = 0.8
+            param['ending_range'] = 2
+            param['scale_search'] = [0.5, 1, 1.5, 2]
+            param['thre1'] = 0.1
+            param['thre2'] = 0.05
+            param['thre3'] = 0.5
+            param['mid_num'] = 4
+            param['min_num'] = 10
+            param['crop_ratio'] = 2.5
+            param['bbox_ratio'] = 0.25
+            param['GPUdeviceNumber'] = 3
+            
             for part in range(17-1):
+                x_list = []
+                y_list = []
                 map_ori = heatmap_avg[:,:,part]
                 map = gaussian_filter(map_ori, sigma=3)
 
@@ -185,8 +200,8 @@ def greengrass_infinite_infer_run():
                 map_down = np.zeros(map.shape)
                 map_down[:,:-1] = map[:,1:]
 
-                peaks_binary = np.logical_and.reduce((map>=map_left, map>=map_right, map>=map_up, map>=map_down, map > 0.1))
-                peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0])) # note reverse
+                peaks_binary = np.logical_and.reduce((map>=map_left, map>=map_right, map>=map_up, map>=map_down, map > param['thre1']))
+                peaks = zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]) # note reverse
                 peaks_with_score = [x + (map_ori[x[1],x[0]],) for x in peaks]
                 id = range(peak_counter, peak_counter + len(peaks))
                 peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
