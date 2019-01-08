79cfdc36c80204bb1d2b660dd8bfee5eaa94980c
githubmg <maria.gaska@gmail.com>
2019-01-08 17:36:35 +0000

    pprint

--- punch.py
+++ punch.py
@@ -18,6 +18,7 @@ from threading import Thread
 #from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 from scipy.ndimage.filters import gaussian_filter
 from PIL import Image
+from pprint import pprint
 
 
 #from pygame import mixer
@@ -182,7 +183,36 @@ def greengrass_infinite_infer_run():
             
             dst = scaledImg
             dst[:,:,2] = dst[:,:,2]+ (heatmap_avg[:,:,15]+0.5)/2*255
+            all_peaks = []
+            peak_counter = 0
 
+            for part in range(17-1):
+                x_list = []
+                y_list = []
+                map_ori = heatmap_avg[:,:,part]
+                map = gaussian_filter(map_ori, sigma=3)
+
+                map_left = np.zeros(map.shape)
+                map_left[1:,:] = map[:-1,:]
+                map_right = np.zeros(map.shape)
+                map_right[:-1,:] = map[1:,:]
+                map_up = np.zeros(map.shape)
+                map_up[:,1:] = map[:,:-1]
+                map_down = np.zeros(map.shape)
+                map_down[:,:-1] = map[:,1:]
+
+                peaks_binary = np.logical_and.reduce((map>=map_left, map>=map_right, map>=map_up, map>=map_down, map > \ 
+                                                      param['thre1']))
+                peaks = zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0]) # note reverse
+                peaks_with_score = [x + (map_ori[x[1],x[0]],) for x in peaks]
+                id = range(peak_counter, peak_counter + len(peaks))
+                peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
+
+                all_peaks.append(peaks_with_score_and_id)
+                peak_counter += len(peaks)
+            
+            print 'allpeaks'
+            pprint(all_peaks)
 
             
 #    except Exception as e:
